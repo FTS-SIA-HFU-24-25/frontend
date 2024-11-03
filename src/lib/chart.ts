15f42e126d4efd9868ef1454c94ce7b19eb8a21d
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2024-11-03 19:16:43 +0100

    [UPDATE] added fully working ecg graph

--- src/lib/chart.ts
+++ src/lib/chart.ts
@@ -13,12 +13,30 @@ export function createChart(id: string, data: ChartData[]): Chart<"line", any, u
 			labels: data.map((d: ChartData) => d.timestamp),
 			datasets: [
 				{
-					label: "",
+					label: "Herzfrequenz",
 					data: data.map((d: ChartData) => d.data),
+					pointRadius: 0,
+					borderColor: "#be123c"
 				}
 			]
+		},
+		options: {
+			scales: {
+				x: {
+					ticks: {
+						callback: function(value, index, _ticks) {
+							if (index === 0) {
+								return this.getLabelForValue(value as number); // Only show labels at start, middle, and end
+							}
+							return '';
+						}
+					}
+				},
+			}
 		}
 	})
+
+	chart.options.animation = false; // disables all animations
 
 	return chart
 }
@@ -26,7 +44,7 @@ export function createChart(id: string, data: ChartData[]): Chart<"line", any, u
 export function addDataToChart(chart: Chart<"line", any, unknown>, data: ChartData) {
 	chart.data.labels?.push(data.timestamp)
 	chart.data.datasets[0].data.push(data.data)
-	if(chart.data.datasets[0].data.length % 23 === 0) {
+	if(chart.data.datasets[0].data.length % 100 === 0) {
 		chart.data.labels = chart.data.labels?.slice(1, -1)
 		chart.data.datasets[0].data = chart.data.datasets[0].data.slice(1, -1)
 	}
